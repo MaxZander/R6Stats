43862738537ad722fd8064db039e0afa08f8f503
Marvin Bosst <marvin.bosst@gmail.com>
2018-09-26 03:39:59 +0200

    Ajoutez des fichiers projet.

---
+++ R6DB Soft/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using API;
+
+namespace R6DB_Soft
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void UDStats()
+        {
+            try
+            {
+                string url = "";
+                string jsonString = await AccessTheWebAsync(url);
+                var stats = Stats.FromJson(jsonString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+
+            }
+        }
+
+        async Task<string> AccessTheWebAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync(url);
+            string urlContents = await getStringTask;
+            return urlContents;
+        }
+
+        private void test_Click(object sender, RoutedEventArgs e)
+        {
+            UDStats();
+        }
+    }
+}
---
+++ Stats/Stats.cs
@@ -0,0 +1,116 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using QuickType;
+//
+//    var stats = Stats.FromJson(jsonString);
+
+namespace API
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Stats
+    {
+        [JsonProperty("player")]
+        public Player Player { get; set; }
+    }
+
+    public partial class Player
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("platform")]
+        public string Platform { get; set; }
+
+        [JsonProperty("ubisoft_id")]
+        public string UbisoftId { get; set; }
+
+        [JsonProperty("indexed_at")]
+        public DateTimeOffset IndexedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [JsonProperty("stats")]
+        public StatsClass Stats { get; set; }
+    }
+
+    public partial class StatsClass
+    {
+        [JsonProperty("ranked")]
+        public Casual Ranked { get; set; }
+
+        [JsonProperty("casual")]
+        public Casual Casual { get; set; }
+
+        [JsonProperty("overall")]
+        public Dictionary<string, long> Overall { get; set; }
+
+        [JsonProperty("progression")]
+        public Progression Progression { get; set; }
+    }
+
+    public partial class Casual
+    {
+        [JsonProperty("has_played")]
+        public bool HasPlayed { get; set; }
+
+        [JsonProperty("wins")]
+        public long Wins { get; set; }
+
+        [JsonProperty("losses")]
+        public long Losses { get; set; }
+
+        [JsonProperty("wlr")]
+        public double Wlr { get; set; }
+
+        [JsonProperty("kills")]
+        public long Kills { get; set; }
+
+        [JsonProperty("deaths")]
+        public long Deaths { get; set; }
+
+        [JsonProperty("kd")]
+        public double Kd { get; set; }
+
+        [JsonProperty("playtime")]
+        public double Playtime { get; set; }
+    }
+
+    public partial class Progression
+    {
+        [JsonProperty("level")]
+        public long Level { get; set; }
+
+        [JsonProperty("xp")]
+        public long Xp { get; set; }
+    }
+
+    public partial class Stats
+    {
+        public static Stats FromJson(string json) => JsonConvert.DeserializeObject<Stats>(json, API.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Stats self) => JsonConvert.SerializeObject(self, API.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
+
